2948236e71ec74f2627486fb8b1e7188f5ac2ad7
Christian Heiberg Hallas <christianhallas@g.harvard.edu>
2020-11-15 22:58:10 -0500

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.13

---
+++ src/BeamPropagation.jl
@@ -0,0 +1,5 @@
+module BeamPropagation
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using BeamPropagation
+using Test
+
+@testset "BeamPropagation.jl" begin
+    # Write your tests here.
+end
